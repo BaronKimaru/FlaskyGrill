abf21bd5d2db538d058af3839fe778cb50bbc1b6
BaronKimaru <bizzinali@gmail.com>
2019-08-03 09:23:00 +0300

    Update test_restaurants.py

--- flaskygrillsrc/app/test_restaurants.py
+++ flaskygrillsrc/app/test_restaurants.py
@@ -1,4 +1,12 @@
 from unittest import TestCase
+from app import models
 
 class RestaurantTestCase(TestCase):
-    pass
+    def setUp(self):
+        config_name="testing"
+        self.app = create_app(config_name)
+        self.client = self.app.test_client
+        self.restaurant  = {'name':'Bhaandini Indian Restaurant'}
+        
+        with self.app.app_context():
+            db.create_all()
